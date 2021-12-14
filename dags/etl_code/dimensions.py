34a3b7b8b2d581429377ec5d839acafffa66d0e8
Ricardo Mendoza <rmendoza@healthcare.com>
2021-12-14 00:12:45 -0600

    Dimensions and fact creation

--- dags/etl_code/dimensions.py
+++ dags/etl_code/dimensions.py
@@ -4,6 +4,8 @@ from sqlalchemy import engine
 
 logger = get_logger()
 
+import pandas as pd
+
 
 @dataclass
 class RegionDimension:
@@ -11,6 +13,24 @@ class RegionDimension:
 
     def build_dimension(self):
         logger.info('Starting building region dimension')
+        query = """
+            INSERT INTO d_region (province_state, country_region, lat, `long`)
+            SELECT province_state, country_region, lat, `long`
+            FROM (
+                SELECT country_region, ifnull(province_state, 'UNK') as province_state, lat, `long`
+                    FROM landing_confirmed
+                UNION
+                SELECT country_region, ifnull(province_state, 'UNK') as province_state,lat, `long`
+                    FROM landing_recovered
+                UNION
+                SELECT country_region, ifnull(province_state, 'UNK') as province_state,lat, `long`
+                    FROM landing_deaths
+            ) unique_regions;
+        """
+
+        with self.db_con.begin() as trans:
+            trans.execute('truncate d_region;')
+            trans.execute(query)
 
 @dataclass
 class DateDimension:
@@ -18,3 +38,30 @@ class DateDimension:
 
     def build_dimension(self):
         logger.info('Starting building region dimension')
+        get_dates = """
+            SELECT MIN(min_date) as min_date, max(max_date) as max_date
+            FROM (
+                SELECT MIN(date) as min_date, MAX(date) as max_date
+                FROM landing_deaths
+                union
+                SELECT MIN(date) as min_date, MAX(date) as max_date
+                FROM landing_recovered
+                union
+                SELECT MIN(date) as min_date, MAX(date) as max_date
+                FROM landing_confirmed
+            ) dates;
+        """
+        query = """
+           CALL GenerateCalendar('{0}', '{1}');
+        """
+
+        with self.db_con.begin() as trans:
+            dates = pd.read_sql(get_dates, con=trans)
+            print(dates)
+            if len(dates.index) == 1:
+                trans.execute('truncate d_date;')
+                trans.execute(query.format(dates.iloc[0, 0], dates.iloc[0, 1]))
+            else:
+                raise ValueError('No dates found in landing tables')
+
+                
--- dags/etl_code/fact_consolidation.py
+++ dags/etl_code/fact_consolidation.py
@@ -10,4 +10,48 @@ class FactCovid:
     db_con: engine
 
     def build_fact_table(self):
-        logger.info('Starting building fact covid table')
+
+        with self.db_con.begin() as trans:
+            trans.execute('truncate f_covid;')
+            trans.execute('drop table if exists landing;')
+            logger.info('Creating landing')
+            trans.execute("""
+                create temporary table landing as
+                select *
+                    , 'confirmed' as source
+                from landing_confirmed
+                union all
+                select *
+                    , 'recovered'as source
+                from landing_recovered
+                union all
+                select *
+                    , 'deaths' as source
+                from landing_deaths;
+            """)
+            logger.info('Creating resumed')
+            trans.execute('drop table if exists resumed;')
+            trans.execute("""
+                create temporary table resumed as
+                select date
+                    , country_region
+                    , province_state
+                    , sum(case when source = 'confirmed' then n_cases else 0 end) as confirmed_cases
+                    , sum(case when source = 'deaths' then n_cases else 0 end) as death_cases
+                    , sum(case when source = 'recovered' then n_cases else 0 end) as recovered_cases
+                from landing
+                group by 1, 2, 3;
+            """)
+            logger.info('Inserting into Fact Table')
+            trans.execute("""
+                insert into f_covid (sk_region, sk_date, confirmed_cases, death_cases, recovered_cases)
+                select dr.sk_region
+                    , dd.sk_date
+                    , ld.confirmed_cases
+                    , ld.death_cases
+                    , ld.recovered_cases
+                from resumed ld
+                left join d_region dr on ld.country_region = dr.country_region
+                    and ld.province_state = dr.province_state
+                left join d_date dd on ld.date = dd.date;
+            """)
