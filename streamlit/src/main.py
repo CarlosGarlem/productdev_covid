4515744558eae5f0710c6889130856e8dfa19969
cgarcia <carlosgarlem@gmail.com>
2021-12-16 08:29:00 -0600

    switch main file

--- streamlit/src/main.py
+++ streamlit/src/main.py
@@ -1,97 +1,121 @@
 import streamlit as st
-import mysql.connector as mysql
+import plotly.express as px
+import time
+from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
 from datetime import datetime
 import sys
 
 
 
-
-db = mysql.connect(
-    host = "db",
-    user = "covid",
-    passwd = "covid123",
-    database = "dm_covid"
-)
-print(db) # it will print a connection object if everything is fine
-cursor = db.cursor()
-
-def run_query(query):
-    cursor.execute(query)
-    return cursor.fetchall()
+#region Global Constants
+SQL_INIT_SCRIPT = 'SELECT * FROM dm_covid.covid_view;'
+#endregion
 
 
-st.set_page_config(page_title="Streamlit COVID Dashboard", layout="wide")
-st.title("COVID-19 Dashboard")
+#region Functions
+@st.cache(allow_output_mutation = True)
+def get_connection():
+    return create_engine('mysql+mysqlconnector://covid:covid123@db/dm_covid')
 
+
+@st.cache(suppress_st_warning = True)
+def load_data(SQL_script):
+    with st.spinner('Cargando datos...'):
+        time.sleep(0.2)
+        df = (pd.read_sql_query(SQL_script, get_connection())
+            .assign(date = lambda df: pd.to_datetime(df.date))
+        )
+    return df
+
+
+
+def getMap(region_df):
+
+    '''
+    fig = px.scatter_geo(plot_region
+                        ,lat = 'lat'
+                        ,lon = 'long'
+                        ,color = 'confirmed'
+                        ,size='confirmed' 
+                        ,template = 'ggplot2'
+                        ,color_continuous_scale='blugrn'
+                        ,range_color = (0,10)
+                        ,hover_name = 'province_state'
+                        #,size_max = 20
+                        ,title = 'Covid Confirmed Cases WorldWide'
+                        ,width = 1800
+                        ,scope = 'world'
+                        ,hover_data = ['confirmed', 'deaths', 'recovered']
+                        ,projection = 'robinson'
+                        #,animation_frame='sk_month'
+                        )
+    '''
+
+    
+    fig = px.choropleth(region_df
+                    ,locations = 'country_region'
+                    ,color='confirmed'
+                    ,color_continuous_scale = 'sunset'
+                    ,hover_data = ['confirmed', 'deaths', 'recovered']
+                    ,hover_name = 'country_region'
+                    ,title = 'Covid Confirmed Cases WorldWide'
+                    ,locationmode='country names'
+                  )
+
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+
+#endregion
+
+
+#region General Data
+st.set_page_config(page_title = 'Streamlit COVID Dashboard', layout = 'wide')
+st.title('COVID-19 Dashboard')
+
+covid_df = load_data(SQL_INIT_SCRIPT)
+
+countries = ['All']
+countries.extend(covid_df['country_region'].unique().tolist())
+#endregion
+
+
+#region SidebarFilters
 st.sidebar.header('Covid Dashboard filters')
-#st.sidebar.markdown("## Dashboard sidebar ")
-query = "SELECT MIN(lat) FROM d_region"
-lat_min = run_query(query)
-lat_max = run_query(query.replace("MIN(", "MAX("))
-latitude_range = st.sidebar.slider("Latitud:", value=[lat_min[0][0], lat_max[0][0]])
-
-query = "SELECT MIN(r.long) FROM d_region r"
-long_min = run_query(query)
-long_max = run_query(query.replace("MIN(", "MAX("))
-longitude_range = st.sidebar.slider("Longitud:", value=[long_min[0][0], long_max[0][0]])
-
-query = "SELECT DISTINCT(country_region) FROM d_region"
-countries = run_query(query)
-countries = [country[0] for country in countries]
-country = st.sidebar.selectbox('País:', countries)
-
-query = "SELECT MIN(date) FROM d_date"
-start_date = run_query(query)
-end_date = run_query(query.replace("MIN(", "MAX("))
-range_dates = st.sidebar.slider('Fechas:', value=[start_date[0][0], end_date[0][0]])
+with st.sidebar:
+    #province_selector = st.selectbox('Choose a Province/State', covid_df['province_state'].unique())
+    country_selector = st.selectbox('Seleccione un Pais/Region:', countries)
+    date_range = st.date_input('Seleccione una fecha:'
+                        , value = (covid_df['date'].min(), covid_df['date'].max())
+                        , min_value=covid_df['date'].min()
+                        , max_value=covid_df['date'].max()
+    )
+    #date_range = st.sidebar.slider('Fechas:', value=[covid_df['date'].min(), covid_df['date'].max()])
+#endregion
 
 
 
-col1, col2, col3 = st.columns(3)
 
-with col1:
-    query = "SELECT FORMAT(SUM(t.cc), 0) FROM (SELECT sk_region, MAX(confirmed_cases) cc FROM f_covid GROUP BY sk_region) t"
-    result = run_query(query)
-    st.markdown("**Casos confirmados**")
-    st.markdown(f"<h1 style='text-align:right; color:#ffde24;'>{result[0][0]}</h1>", unsafe_allow_html=True)
+#region Streamlit Dash
+region_df = (covid_df.groupby(['country_region', 'date'], as_index = False)
+            .agg(confirmed = ('confirmed_cases', np.max), deaths = ('death_cases', np.max), recovered = ('recovered_cases', np.max))
+            .loc[lambda df: (df.date.dt.date == date_range[0]) | (df.date.dt.date == date_range[1])]
+            .sort_values(by = ['country_region', 'date'])
+            .assign(diff_confirmed = lambda df: df.groupby(['country_region'])['confirmed'].diff()
+                   ,diff_deaths = lambda df: df.groupby(['country_region'])['deaths'].diff()
+                   ,diff_recovered = lambda df: df.groupby(['country_region'])['recovered'].diff()
+            )
+            .dropna(axis = 0, subset = ['diff_confirmed', 'diff_deaths', 'diff_recovered'])
+            .iloc[:, 0:-3]
+            .drop(axis = 1, labels = 'date')
+)
+if country_selector != 'All':
+    region_df = region_df.loc[lambda df: (df.country_region == country_selector)]
 
-with col2:
-    query = "SELECT FORMAT(SUM(t.rc), 0) FROM (SELECT sk_region, MAX(recovered_cases) rc FROM f_covid GROUP BY sk_region) t"
-    result = run_query(query)
-    st.markdown("**Casos recuperados**")
-    st.markdown(f"<h1 style='text-align:right; color:#00ad00;'>{result[0][0]}</h1>", unsafe_allow_html=True)
-
-
-with col3:
-    query = "SELECT FORMAT(SUM(t.dc), 0) FROM (SELECT sk_region, MAX(death_cases) dc FROM f_covid GROUP BY sk_region) t"
-    result = run_query(query)
-    st.markdown("**Fallecimientos**")
-    st.markdown(f"<h1 style='text-align:right; color:red;'>{result[0][0]}</h1>", unsafe_allow_html=True)
-
-
-data1, data2 = st.columns(2)
-
-with data1:
-    query = "SELECT r.country_region, r.sk_region, MAX(f.confirmed_cases) cc FROM f_covid f, d_region r WHERE f.sk_region = r.sk_region GROUP BY r.country_region, r.sk_region"
-    query = "SELECT t.country_region, FORMAT(SUM(t.cc), 0) confirmed, SUM(t.cc) FROM (" + query + ") t GROUP BY t.country_region ORDER BY 3 DESC LIMIT 10"
-    result = run_query(query)
-    result = [item[:2] for item in result]
-    st.subheader("Top 10 casos confirmados")
-    df = pd.DataFrame(result, columns=['País', 'Casos'])
-    df
-
-with data2:
-    st.subheader("Casos confirmados")
-    chart_data = None
-    if (range_dates and country):
-        query = "SELECT date_format(d.date, '%y-%m-%d'), f.confirmed_cases, f.death_cases, f.recovered_cases FROM f_covid f \
-                INNER JOIN d_date d ON f.sk_date = d.sk_date AND d.date between '{}' and '{}' \
-                INNER JOIN d_region r ON f.sk_region = r.sk_region AND r.country_region = '{}'".format(range_dates[0].isoformat(), range_dates[1].isoformat(), country)
-        chart_data = run_query(query)
-    df = pd.DataFrame(chart_data, columns=['date', 'confirmed', 'death', 'recovered'])
-    df = df.set_index('date')
-    st.line_chart(df)
+st.plotly_chart(getMap(region_df), use_container_width=True)
+region_df
 
 
-
+#endregion
